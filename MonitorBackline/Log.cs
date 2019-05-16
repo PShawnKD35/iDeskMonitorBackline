29cc26652039f9985b63d320980350eafb7eb0c8
Shawn Peng <lankeypeng@163.com>
2019-05-16 09:46:54 +0800

    [Add] Added Log class and updated query for Multichannel.

---
+++ MonitorBackline/Log.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonitorBackline
+{
+    static class Log
+    {
+        const string DATETIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+        /// <summary>
+        /// Write Log with a Timestamp
+        /// </summary>
+        /// <param name="log"></param>
+        public static void WriteLine(string log)
+        {
+            Console.WriteLine(string.Format("{0} - {1}", DateTime.Now.ToString(DATETIME_FORMAT), log));
+        }
+
+    }
+}
--- MonitorBackline/Program.cs
+++ MonitorBackline/Program.cs
@@ -15,7 +15,7 @@ namespace MonitorBackline
                 soundPlayer.Load();
             }catch(Exception ex)
             {
-                Console.WriteLine(string.Format("Mission aborted! Unable to load notificaiont sound file '{0}':{1}{2}", soundPlayer.SoundLocation, Environment.NewLine, ex.Message));
+                Log.WriteLine(string.Format("Mission aborted! Unable to load notificaiont sound file '{0}':{1}{2}", soundPlayer.SoundLocation, Environment.NewLine, ex.Message));
                 Console.WriteLine("Press any key to close...");
                 Console.ReadKey();
                 return;
@@ -25,28 +25,31 @@ namespace MonitorBackline
             string username = Console.ReadLine();
             Console.WriteLine("Please input your iDesk password:");
             string password = InputPassword.ReadLine();
+            Console.WriteLine();
 
-            IDeskRestAPI idesk = new IDeskRestAPI(username, password, null, IDeskRestAPI.ENV_PROD, 10, null, log => Console.WriteLine(DateTime.Now.ToShortTimeString() + " - " + log));
+            IDeskRestAPI idesk = new IDeskRestAPI(username, password, null, IDeskRestAPI.ENV_PROD, 10, null, log => Log.WriteLine(log));
 
+            Log.WriteLine("Start to monitor Multichannel-Backline");
 
             while (true)
             {
                 //Login
                 if (!Task.Run(() => idesk.LoginAsync()).Result)
                 {
-                    Console.WriteLine("Login failed.");
+                    Log.WriteLine("Login failed.");
                     Console.WriteLine("Press any key to close...");
                     Console.ReadKey();
                     return;
                 }
-                Console.WriteLine("Start to monitor Multichannel-Backline");
+
 #if DEBUG
                 Console.WriteLine(DateTime.Now.ToShortTimeString() + " - Logged in.");
 #endif
 
                 //Search queue
                 //string q = @"((('Assigned Group' LIKE ""Frontline_%"" OR 'Assigned Group' LIKE ""%BacklineITSD"") AND (('ServiceCI' = ""order management"" OR 'ServiceCI' = ""cwis (central warehousing information service)"" OR 'ServiceCI' = ""astro [warehouse management system]"" OR 'ServiceCI' = ""astro"" OR 'ServiceCI' = ""ymm (yard management)"" OR 'ServiceCI' = ""centiro"" OR 'ServiceCI' = ""centiro application"")OR ('Description' LIKE ""MCS"" AND NOT ('ServiceCI' = ""isell"" OR 'ServiceCI' = ""isell application""))))AND ('Status' < ""Pending"" OR ('Status' = ""Resolved"" AND 'Status_Reason' =""Updated By Mail""))AND ('Country' = ""Australia""OR 'Country' = ""Austria"" OR 'Country' = ""Belgium""OR 'Country' = ""China"" OR 'Country' = ""Denmark""OR 'Country' = ""Finland""OR 'Country' = ""France"" OR 'Country' = ""Germany""OR 'Country' = ""Ireland""OR 'Country' = ""Italy"" OR 'Country' = ""Netherlands"" OR 'Country' = ""Norway""OR 'Country' = ""Poland""OR 'Country' = ""Portugal"" OR 'Country' = ""South Korea""OR 'Country' = ""Spain"" OR 'Country' = ""Sweden"" OR 'Country' = ""Switzerland"" OR 'Country' = ""United Kingdom""))";
-                string q = @"((('Assigned Group' LIKE ""%BacklineITSD"") AND (('ServiceCI' = ""order management"" OR 'ServiceCI' = ""cwis (central warehousing information service)"" OR 'ServiceCI' = ""astro [warehouse management system]"" OR 'ServiceCI' = ""astro"" OR 'ServiceCI' = ""ymm (yard management)"" OR 'ServiceCI' = ""centiro"" OR 'ServiceCI' = ""centiro application"")OR ('Description' LIKE ""MCS"" AND NOT ('ServiceCI' = ""isell"" OR 'ServiceCI' = ""isell application""))))AND ('Status' < ""Pending"" OR ('Status' = ""Resolved"" AND 'Status_Reason' =""Updated By Mail""))AND ('Country' = ""Australia""OR 'Country' = ""Austria"" OR 'Country' = ""Belgium""OR 'Country' = ""China"" OR 'Country' = ""Denmark""OR 'Country' = ""Finland""OR 'Country' = ""France"" OR 'Country' = ""Germany""OR 'Country' = ""Ireland""OR 'Country' = ""Italy"" OR 'Country' = ""Netherlands"" OR 'Country' = ""Norway""OR 'Country' = ""Poland""OR 'Country' = ""Portugal"" OR 'Country' = ""South Korea""OR 'Country' = ""Spain"" OR 'Country' = ""Sweden"" OR 'Country' = ""Switzerland"" OR 'Country' = ""United Kingdom""))";
+                //string q = @"((('Assigned Group' LIKE ""%BacklineITSD"") AND (('ServiceCI' = ""order management"" OR 'ServiceCI' = ""cwis (central warehousing information service)"" OR 'ServiceCI' = ""astro [warehouse management system]"" OR 'ServiceCI' = ""astro"" OR 'ServiceCI' = ""ymm (yard management)"" OR 'ServiceCI' = ""centiro"" OR 'ServiceCI' = ""centiro application"")OR ('Description' LIKE ""MCS"" AND NOT ('ServiceCI' = ""isell"" OR 'ServiceCI' = ""isell application""))))AND ('Status' < ""Pending"" OR ('Status' = ""Resolved"" AND 'Status_Reason' =""Updated By Mail""))AND ('Country' = ""Australia""OR 'Country' = ""Austria"" OR 'Country' = ""Belgium""OR 'Country' = ""China"" OR 'Country' = ""Denmark""OR 'Country' = ""Finland""OR 'Country' = ""France"" OR 'Country' = ""Germany""OR 'Country' = ""Ireland""OR 'Country' = ""Italy"" OR 'Country' = ""Netherlands"" OR 'Country' = ""Norway""OR 'Country' = ""Poland""OR 'Country' = ""Portugal"" OR 'Country' = ""South Korea""OR 'Country' = ""Spain"" OR 'Country' = ""Sweden"" OR 'Country' = ""Switzerland"" OR 'Country' = ""United Kingdom""))";
+                string q = @"(((('Assigned Group' LIKE ""Frontline_%"" OR 'Assigned Group' LIKE ""%BacklineITSD"") AND (('ServiceCI' = ""nwp (new web platform)"" OR 'ServiceCI' = ""atg (oracle atg web commerce)"" OR 'ServiceCI' = ""wcs (web center sites)"" OR 'ServiceCI' = ""iwf (ikea web frontend)"" OR 'ServiceCI' = ""endeca"" OR 'ServiceCI' = ""spl (service proxy layer)"" OR 'ServiceCI' = ""iwc (ikea web co-worker tool)"" OR 'ServiceCI' = ""icm (ikea customer master)"" OR 'ServiceCI' = ""icm - och (oracle customer hub)"" OR 'ServiceCI' = ""icm - oedq (oracle enterprise data quality)"" OR 'ServiceCI' = ""icma (icm analytics)"" OR 'ServiceCI' = ""icm - analytics"" OR 'ServiceCI' = ""isom (ikea sales order management)"" OR 'ServiceCI' = ""isom application"" OR 'ServiceCI' = ""soia - sales order integration adapter"" OR 'ServiceCI' = ""isom reporting"" OR 'ServiceCI' = ""isom reporting application""OR 'ServiceCI' = ""isom reporting cognos""OR 'ServiceCI' = ""isom analytics"" OR 'ServiceCI' = ""isom analytics application""OR 'ServiceCI' = ""isom analytics cognos""OR 'ServiceCI' = ""isom infrastructure"") OR ('Owner Group' = ""Multichannel-BacklineITSD"")OR (('ServiceCI' = ""isell"" OR 'ServiceCI' = ""isell application"")AND'Description' LIKE ""MCS"")))OR ('Assigned Group' = ""Multichannel-BacklineITSD""))AND (('Status' < ""Resolved"") OR ('Status' = ""Resolved"" AND 'Status_Reason' = ""Updated By Mail""))) AND 'Status' != ""Pending""";
                 IDeskSearchResult result = Task.Run(() => idesk.SearchIncidents(q, IDeskRestAPI.FIELDS_DEFAULT, IDeskRestAPI.SORT_DEFAULT)).Result;
 
                 //Logout
@@ -74,12 +77,12 @@ namespace MonitorBackline
                         Console.WriteLine(sb.ToString());
                     }                    
                     soundPlayer.PlayLooping();
-                    Console.WriteLine("Enter any key to stop the sound and continue...");
+                    Console.WriteLine("Press any key to stop the sound and continue...");
                     Console.ReadKey();
                     soundPlayer.Stop();
                 }else
                 {
-                    Console.WriteLine("No cases found.");
+                    Log.WriteLine("No cases found.");
                 }
 
                 //Repeat every 5 mins
